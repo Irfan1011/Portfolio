3f1feae2c596ac578ca5e1f9163475a4e3bbce17
Irfan <irfan10112001@gmail.com>
2024-01-30 17:01:22 +0700

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Portfolio/'
-    : '/',
+  publicPath: '/Portfolio',
   transpileDependencies: true
 })
